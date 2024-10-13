a4d4699d9773cf2bba61a313b8444c7597c6dbc4
Rotem Horesh <horesh.rotem@gmail.com>
2024-10-13 22:45:16 +0300

    initial commit

---
+++ .config/nvim/lua/rotemhoresh/plugins/treesitter.lua
@@ -0,0 +1,34 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "go",
+        "gomod",
+        "gosum",
+        "templ",
+        "make",
+        "json",
+        "yaml",
+        "bash",
+      },
+      sync_install = false,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false
+      },
+      indent = { enable = true },
+    })
+  end
+}
